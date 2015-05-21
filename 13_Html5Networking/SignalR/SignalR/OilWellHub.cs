30fcce5e01d23c090a7e8a41b91abefa3b4bbc2a
Brock Allen <brockallen@gmail.com>
2015-05-21 16:50:21 -0400

    forgot signalr lab

---
+++ 13_Html5Networking/SignalR/SignalR/OilWellHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR
+{
+    [HubName("OilWell")]
+    public class OilWellHub : Hub
+    {
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            //this.Context.ConnectionId;
+            //this.Context.User;
+            //this.Groups.Add(this.Context.ConnectionId, "Emergency");
+            return base.OnConnected();
+        }
+
+        public void StartDrilling(string name)
+        {
+            this.Clients.Others.DrillingStarted(name);
+        }
+    }
+}
---
+++ 13_Html5Networking/SignalR/SignalR/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(SignalR.Startup))]
+
+namespace SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+
+            Task.Factory.StartNew(async () =>
+            {
+                while(true)
+                {
+                    await Task.Delay(2000);
+                    var hub = GlobalHost.ConnectionManager.GetHubContext<OilWellHub>();
+                    hub.Clients.All.DrillingStarted("spam " + DateTime.Now);
+                }
+            });
+        }
+    }
+}
