3f05927fb0769bdc960258cb03820be54f6ac474
Brock Allen <brockallen@gmail.com>
2016-03-08 16:03:16 -0600

    angular services demo

---
+++ 06_AngularServices/WebApplication1/WebApplication1/DirectorsMod.js
@@ -0,0 +1,98 @@
+﻿/// <reference path="angular.js" />
+
+
+(function () {
+    var directors = [
+           { id: 1, name: "Bob" },
+           { id: 5, name: "Alice" }
+    ];
+
+    var mod = angular.module("DirectorsMod", []);
+
+    mod.filter("directorName", function (DirectorService) {
+        return function (id) {
+            //var matching = directors.filter(function (d) {
+            //    return d.id === id;
+            //});
+            //return matching[0] && matching[0].name;
+            var d = DirectorService.getById(id);
+            return d && d.name;
+        };
+    });
+
+    mod.config(function (DirectorServiceProvider, HttpProvider) {
+        DirectorServiceProvider.logging = true;
+    });
+
+    mod.run(function (DirectorService) {
+
+    });
+
+    mod.provider("DirectorService", function () {
+        var provider = {
+            logging: false,
+
+            $get: function () {
+                var svc = {
+                    getAll: function () {
+                        if (provider.logging) {
+                            console.log("logging....");
+                        }
+                        return directors;
+                    },
+                    getById: function (id) {
+                        if (provider.logging) {
+                            console.log("getting id", id);
+                        }
+
+                        var matching = directors.filter(function (d) {
+                            return d.id === id;
+                        });
+                        return matching[0];
+                    },
+                    update: function (director) {
+                        console.log("updating director", director);
+                    }
+                };
+                return svc;
+            }
+        };
+        return provider;
+    });
+
+    //mod.service("DirectorService", function () {
+    //    this.getAll = function () {
+    //        return directors;
+    //    };
+    //    this.getById = function (id) {
+    //        var matching = directors.filter(function (d) {
+    //            return d.id === id;
+    //        });
+    //        return matching[0];
+    //    };
+    //    this.update = function (director) {
+    //        console.log("updating director", director);
+    //    };
+    //});
+
+    mod.factory("DirectorService", function () {
+
+        var logging = true;
+
+        var svc = {
+            getAll: function () {
+                return directors;
+            },
+            getById: function (id) {
+                var matching = directors.filter(function (d) {
+                    return d.id === id;
+                });
+                return matching[0];
+            },
+            update: function (director) {
+                console.log("updating director", director);
+            }
+        };
+        return svc;
+    });
+})();
---
+++ 06_AngularServices/WebApplication1/WebApplication1/app.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="angular.js"/>
+
+(function () {
+
+    var mod = angular.module("app", ["DirectorsMod"]);
+
+    mod.controller("MovieCtrl", function ($scope, DirectorService) {
+        $scope.name = "Aliens";
+        $scope.rating = "R";
+        $scope.director = 5;
+
+        //$scope.DirectorService = DirectorService;
+
+        $scope.$watch("name", function (newValue, oldValue) {
+            //console.log(newValue, oldValue);
+        });
+
+        //$scope.getDirectorName = function (id) {
+        //    var matching = directors.filter(function (d) {
+        //        return d.id === id;
+        //    });
+        //    return matching[0] && matching[0].name;
+        //}
+
+        $scope.getDirectorName = function (id) {
+            var d = DirectorService.getById(id);
+            return d && d.name;
+        }
+    });
+
+})();
