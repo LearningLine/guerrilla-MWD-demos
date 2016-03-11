85a50fc3c22621569d99d452c64d55a1310c5b1e
Brock Allen <brockallen@gmail.com>
2016-03-11 17:02:33 -0600

    aspnet core 1

---
+++ 20_dotnet_core/app/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("api/brock")]
+        public IActionResult Ajax()
+        {
+            return new ObjectResult(new
+            {
+                name = "Brock",
+                age = 100
+            });
+        }
+
+
+    }
+}
---
+++ 20_dotnet_core/app/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace app
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //services.AddTransient;
+            //services.AddScoped;
+            //services.AddSingleton;
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute("default",
+            //        "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            //app.Use(async (ctx, next) =>
+            //{
+            //    Console.WriteLine(ctx.Request.Path);
+            //    await ctx.Response.WriteAsync("<h1>jkdfhfdjhkfdello DNX/Kestrel!</h1>");
+            //});
+        }
+    }
+}
