1b1a5530bb5f325fea0af1d6386e730b0d52a594
Brock Allen <brockallen@gmail.com>
2016-03-10 20:45:19 -0600

    signalr

---
+++ 16_SignalR/WebApplication1/WebApplication1/Chat.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Chat : Hub
+    {
+        public override Task OnConnected()
+        {
+            //this.Context.User
+            this.Groups.Add(this.Context.ConnectionId, "cats");
+            return base.OnConnected();
+        }
+
+        public void SendChatMessage(string chat)
+        {
+            //this.Clients.Client("1234-56").GotChatMessage(chat);
+            //this.Clients.Group("cats").GotChatMessage(chat);
+            this.Clients.All.GotChatMessage(chat);
+        }
+    }
+}
